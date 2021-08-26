36b5cf0237c9e19b3ed927151a96d2800710202b
Medha Tyagi <tyagi.25@gmail.com>
2021-08-26 15:22:40 +0100

    First Commit

---
+++ my-app1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myapp1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
